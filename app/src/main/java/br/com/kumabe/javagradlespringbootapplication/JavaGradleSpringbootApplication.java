13ccce087439c4a59e03b6df47663875fd584b13
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-03-05 14:01:17 -0300

    first commit

---
+++ app/src/main/java/br/com/kumabe/javagradlespringbootapplication/JavaGradleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.javagradlespringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGradleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGradleSpringbootApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/com/kumabe/javagradlespringbootapplication/JavaGradleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.javagradlespringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGradleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
