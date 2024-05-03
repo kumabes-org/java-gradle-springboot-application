f110f4f0f3f3f4d06d05f64f7dfa1cbfde7aa62b
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2024-05-03 16:27:27 -0300

    feat: upgrade java 17

--- app/src/main/java/br/com/kumabe/controllers/ContatoController.java
+++ app/src/main/java/br/com/kumabe/controllers/ContatoController.java
@@ -5,8 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
-
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
--- app/src/main/java/br/com/kumabe/models/Contato.java
+++ app/src/main/java/br/com/kumabe/models/Contato.java
@@ -1,13 +1,8 @@
 package br.com.kumabe.models;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "T_CONTATO")
